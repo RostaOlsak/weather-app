995e1552463c4c994838497be780004ef58330a3
RostislavOlsak <rosta.olsak12@gmail.com>
2022-12-29 13:55:11 +0100

    Added basic info + some styling

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,13 +1,58 @@
 import { forecastType } from "../types";
+import Feels from "./Icons/Pocit";
+import Sunrise from "./Icons/Sunrise";
+import Sunset from "./Icons/Sunset";
 
 type Props = {
   forecastData: forecastType;
 };
 
-const Forecast = ({ forecastData }: Props) => {
+const Celsius = ({ temp }: { temp: number }): JSX.Element => (
+  <span>{temp}°C</span>
+);
+
+const Forecast = ({ forecastData }: Props): JSX.Element => {
+  const currentWeather = forecastData.list[0];
   return (
-    <div>
-      <p>Forecast</p>
+    <div className="forecast-container">
+      <div className="forecast-main">
+        <section className="forecast-section-container">
+          <h2 className="forecast-title">
+            {forecastData.name},{" "}
+            <span className="forecast-title-span">{forecastData.country}</span>
+          </h2>
+          <h1 className="forecast-today">
+            <Celsius temp={Math.round(currentWeather.main.temp)} />
+          </h1>
+          <p>
+            {currentWeather.weather[0].main}{" "}
+            {currentWeather.weather[0].description}
+          </p>
+          <p>
+            MAX. <Celsius temp={Math.ceil(currentWeather.main.temp_max)} /> MIN.{" "}
+            <Celsius temp={Math.floor(currentWeather.main.temp_min)} />
+          </p>
+          <section className="forecast-section-scroll">
+            {forecastData.list.map((item, i) => (
+              <div key={i}>
+                <p>{i === 0 ? "Now" : new Date(item.dt * 1000).getHours()}</p>
+                <img
+                  alt={`weather-icon-${item.weather[0].description}`}
+                  src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                />
+                <p>
+                  <Celsius temp={Math.round(item.main.temp)} />
+                </p>
+              </div>
+            ))}
+          </section>
+        </section>
+
+        <section className="additional-data-container">
+          <div className=""><Sunrise /></div>
+          <div className=""><Sunset /></div>
+        </section>
+      </div>
     </div>
   );
 };
